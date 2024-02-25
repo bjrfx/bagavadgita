cf3395e0a09f28dd3f17373c6eb428675ce7bb3d
bjrfx <phanikiran1234@gmail.com>
2024-02-24 23:35:58 -0500

    Express server added
    -Linked react app with express. (working)

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../build')));
+
+// Define other routes or API endpoints here
+
+// Always serve the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
+// Start server
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
